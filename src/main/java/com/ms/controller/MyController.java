3c4fb5dc8ddff3ececb659ddf8352d5545839b94
MS-Raunak <raunakkr.saha@gmail.com>
2024-05-29 01:05:04 +0530

    basic thymleaf

---
+++ src/main/java/com/ms/controller/MyController.java
@@ -0,0 +1,20 @@
+package com.ms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+	
+	//http://localhost:8080/firsthymeleaf
+	
+	@RequestMapping("/firsthymeleaf")
+	public String handler1(Model model) {  //sending data to the view thru model
+		model.addAttribute("name", "Thymeleaf");
+		model.addAttribute("msg", "jai shree ram");
+		return "firstThymeleaf";
+	}
+	
+
+}
