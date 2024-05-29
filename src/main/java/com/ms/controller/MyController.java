71d8b4a51a32dd73cdf3139c438fa096be25c9fc
MS-Raunak <raunakkr.saha@gmail.com>
2024-05-29 13:56:03 +0530

    fragments-Thymleaf

--- src/main/java/com/ms/controller/MyController.java
+++ src/main/java/com/ms/controller/MyController.java
@@ -39,5 +39,11 @@ public class MyController {
 		
 		return "conditional";
 	}
+	
+	//Fragments handler
+	@RequestMapping("/fragments")
+	public String getFragments() {
+		return "fragments";
+	}
 
 }
