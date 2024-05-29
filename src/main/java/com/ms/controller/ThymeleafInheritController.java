0d15b2048b4392321994b0f48794946ed232e929
MS-Raunak <raunakkr.saha@gmail.com>
2024-05-29 19:03:55 +0530

    course-completed

--- src/main/java/com/ms/controller/ThymeleafInheritController.java
+++ src/main/java/com/ms/controller/ThymeleafInheritController.java
@@ -1,15 +1,14 @@
 package com.ms.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import ch.qos.logback.core.model.Model;
 
 @Controller
 public class ThymeleafInheritController {
 	
 	@RequestMapping("/inherits")
-	public String getInherit(org.springframework.ui.Model model) {
+	public String getInherit(Model model) {
 		return "inherit";
 	}
 }
